9f518fdbfb1365f98bf1b12d16a9f8cc92630267
Matias3154 <89808203+Matias3154@users.noreply.github.com>
2021-10-06 16:53:43 +0200

    Trabajo terminado

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,31 +1,19 @@
 import React from "react";
-
+import "./styles/Footer.css"
 const PieDePagina = () => {
-  return <footer>
-    <nav>
-      <div>
-        <a href="/">
-          <img 
-            src="https://play.cine.ar/content/images/98fd6ee3.logo-cinear-estrenos.svg" 
-            alt="foto" 
-          />
-        </a>
-      </div>
-      <div>
-        <span>Todos los derechos reservados 2003-2021. República Argentina. INCAA Lima 319; Código Postal: C1073AAG, CABA; República Argentina.</span>
-        <ul className="PieDePagina">
+  return <footer id = "PieDePagina">
+        <p id = "ParrafoDelFooter">Todos los derechos reservados 2003-2021. República Argentina. <br /> INCAA Lima 319; Código Postal: C1073AAG, CABA; República Argentina.</p>
+        <ul className="EnlacesDePieDePagina">
           <li>
-            <a href="#"><img src="http://cine.ar/img/logo-cinear.svg" alt="" /></a>
+            <a href="/"><img src="http://cine.ar/img/logo-cinear.svg" alt="" /></a>
           </li>
           <li>
-            <a href="#"><img src="http://cine.ar/img/logo-arsat.svg" alt="" /></a>
+            <a href="/"><img src="http://cine.ar/img/logo-arsat.svg" alt="" /></a>
           </li>
           <li>
-            <a href="#"><img src="http://cine.ar/img/logo-incaa.svg" alt="" /></a>
+            <a href="/"><img src="http://cine.ar/img/logo-incaa.svg" alt="" /></a>
           </li>
         </ul>
-      </div>
-    </nav>
   </footer>;
 
 };
